db41b1afde57bd30884190936eea616c1adb4b8a
Bhunesh varma <bhuneshvarma63@gmail.com>
2024-05-08 09:48:39 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const fetchuser = require("../middleware/fetchUser");
+const fetchuser = require("../middleware/fetchuser");
 const JWT_SECRET = 'bhuneshvarmaisthebest';
 
 //ROUTE 1: Create a user using : POST "/api/auth/createuser". No login required
