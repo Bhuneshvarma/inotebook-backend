9e5b09ee7b7f669ba152a832832fbcd9ee4b1d0a
Bhunesh varma <bhuneshvarma63@gmail.com>
2024-05-08 12:20:39 +0530

    first commit

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-undef */
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-
-// const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.0";
-const mongoURI = `mongodb+srv://bhuneshvarma63:VYS91t7bWMYHt9HD@inotebook.zqifxiq.mongodb.net/inotebook`;
+const mongoURI = process.env.MONGODB_URI;
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI);
-        console.log(`Connected to MongoDB successfully ${mongoose.connection.host}`);
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
