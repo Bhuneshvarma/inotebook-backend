98d1a0693dd0b69e8121b252be3ef596b344ff4e
Bhunesh varma <bhuneshvarma63@gmail.com>
2024-05-08 11:02:03 +0530

    first commit

--- db.js
+++ db.js
@@ -2,13 +2,13 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.0";
-// const mongoURI = "mongodb+srv://bhuneshvarma63:<password>@inotebook.lef3k1y.mongodb.net/?retryWrites=true&w=majority&appName=inotebook";
+// const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.0";
+const mongoURI = `mongodb+srv://bhuneshvarma63:VYS91t7bWMYHt9HD@inotebook.zqifxiq.mongodb.net/inotebook`;
 
 const connectToMongo = async () => {
     try {
         await mongoose.connect(mongoURI);
-        console.log("Connected to MongoDB successfully");
+        console.log(`Connected to MongoDB successfully ${mongoose.connection.host}`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
